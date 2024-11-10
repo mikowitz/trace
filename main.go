09df41f4fb046381de946bd8620e6ef275edad9f
Michael Berkowitz <michael.berkowitz@gmail.com>
2024-11-09 19:16:13 -0500

    Dielectrics

--- main.go
+++ main.go
@@ -7,12 +7,14 @@ import (
 func main() {
 	groundMat := trace.Lambertian{Albedo: trace.NewColor(0.8, 0.8, 0)}
 	centerMat := trace.Lambertian{Albedo: trace.NewColor(0.1, 0.2, 0.5)}
-	leftMat := trace.Metal{Albedo: trace.NewColor(0.8, 0.8, 0.8), Fuzz: 0.3}
+	leftMat := trace.Dielectric{RefractionIndex: 1.5}
+	bubbleMat := trace.Dielectric{RefractionIndex: 1.0 / 1.5}
 	rightMat := trace.Metal{Albedo: trace.NewColor(0.8, 0.6, 0.2), Fuzz: 1.0}
 	world := trace.HittableList{}
 	world.Add(trace.NewSphere(trace.NewVec(0, -100.5, -1), 100, &groundMat))
 	world.Add(trace.NewSphere(trace.NewVec(0, 0, -1.2), 0.5, &centerMat))
 	world.Add(trace.NewSphere(trace.NewVec(-1, 0, -1), 0.5, &leftMat))
+	world.Add(trace.NewSphere(trace.NewVec(-1, 0, -1), 0.4, &bubbleMat))
 	world.Add(trace.NewSphere(trace.NewVec(1, 0, -1), 0.5, &rightMat))
 
 	c := trace.NewCamera()
