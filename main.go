d76380353d741ec0b4b0456a8216e13d2e82bb5d
Michael Berkowitz <michael.berkowitz@gmail.com>
2024-11-12 16:15:15 -0500

    a final render (#13)
    * A Final Render
    
    * A Final Render

--- main.go
+++ main.go
@@ -1,34 +1,9 @@
 package main
 
 import (
-	"github.com/mikowitz/trace/pkg/trace"
+	"github.com/mikowitz/trace/pkg/examples"
 )
 
 func main() {
-	groundMat := trace.Lambertian{Albedo: trace.NewColor(0.8, 0.8, 0)}
-	centerMat := trace.Lambertian{Albedo: trace.NewColor(0.1, 0.2, 0.5)}
-	leftMat := trace.Dielectric{RefractionIndex: 1.5}
-	bubbleMat := trace.Dielectric{RefractionIndex: 1.0 / 1.5}
-	rightMat := trace.Metal{Albedo: trace.NewColor(0.8, 0.6, 0.2), Fuzz: 1.0}
-	world := trace.HittableList{}
-	world.Add(trace.NewSphere(trace.NewVec(0, -100.5, -1), 100, &groundMat))
-	world.Add(trace.NewSphere(trace.NewVec(0, 0, -1.2), 0.5, &centerMat))
-	world.Add(trace.NewSphere(trace.NewVec(-1, 0, -1), 0.5, &leftMat))
-	world.Add(trace.NewSphere(trace.NewVec(-1, 0, -1), 0.4, &bubbleMat))
-	world.Add(trace.NewSphere(trace.NewVec(1, 0, -1), 0.5, &rightMat))
-
-	c := trace.NewCamera()
-	c.AspectRatio(16.0 / 9.0)
-	c.ImageWidth(400)
-	c.SamplesPerPixel(100)
-	c.MaxDepth(50)
-	c.Vfov(20.0)
-	c.Lookfrom(trace.NewVec(-2, 2, 1))
-	c.Lookat(trace.NewVec(0, 0, -1))
-	c.Vup(trace.NewVec(0, 1, 0))
-
-	c.DefocusAngle(10.0)
-	c.FocusDist(3.4)
-
-	c.Render(world)
+	examples.WeekendFinal()
 }
---
+++ pkg/examples/weekend_final.go
@@ -0,0 +1,63 @@
+package examples
+
+import (
+	"math/rand"
+
+	"github.com/mikowitz/trace/pkg/trace"
+)
+
+func WeekendFinal() {
+	groundMat := trace.Lambertian{Albedo: trace.NewColor(0.5, 0.5, 0.5)}
+
+	world := trace.HittableList{}
+	world.Add(trace.NewSphere(trace.NewVec(0, -1000, 0), 1000, &groundMat))
+
+	for a := -11; a < 11; a++ {
+		for b := -11; b < 11; b++ {
+			chooseMat := rand.Float64()
+			center := trace.NewVec(float64(a)+0.9*rand.Float64(), 0.2, float64(b)+0.9*rand.Float64())
+
+			if center.Sub(trace.NewVec(4, 0.2, 0)).Length() > 0.9 {
+				var material trace.Material
+
+				if chooseMat < 0.9 {
+					albedo := trace.RandomVec().Prod(trace.RandomVec())
+					material = &trace.Lambertian{Albedo: albedo}
+				} else if chooseMat < 0.95 {
+					albedo := trace.RandomVecIn(0.5, 1)
+					fuzz := rand.Float64() * 0.5
+					material = &trace.Metal{Albedo: albedo, Fuzz: fuzz}
+				} else {
+					material = &trace.Dielectric{RefractionIndex: 1.5}
+				}
+
+				world.Add(trace.NewSphere(center, 0.2, material))
+			}
+		}
+	}
+
+	mat1 := trace.Dielectric{RefractionIndex: 1.5}
+	world.Add(trace.NewSphere(trace.NewVec(0, 1, 0), 1, &mat1))
+
+	mat2 := trace.Lambertian{Albedo: trace.NewColor(0.4, 0.2, 0.1)}
+	world.Add(trace.NewSphere(trace.NewVec(-4, 1, 0), 1, &mat2))
+
+	mat3 := trace.Metal{Albedo: trace.NewColor(0.7, 0.6, 0.5), Fuzz: 0.0}
+	world.Add(trace.NewSphere(trace.NewVec(4, 1, 0), 1, &mat3))
+
+	c := trace.NewCamera()
+	c.AspectRatio(16.0 / 9.0)
+	c.ImageWidth(1200)
+	c.SamplesPerPixel(500)
+	c.MaxDepth(50)
+
+	c.Vfov(20.0)
+	c.Lookfrom(trace.NewVec(13, 2, 3))
+	c.Lookat(trace.NewVec(0, 0, 0))
+	c.Vup(trace.NewVec(0, 1, 0))
+
+	c.DefocusAngle(0.5)
+	c.FocusDist(10.0)
+
+	c.Render(world)
+}
